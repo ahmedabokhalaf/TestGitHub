6cb37d999747a90e560b08dbe2986d2ca2378615
yousefzakaria250 <yousefzakaria27@gmail.com>
2022-11-19 18:10:51 +0200

    Create Sidebar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
