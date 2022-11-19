6f00ff6948b307ed9a13d130924fbaedb8b79920
Ahmed Khalaf Hassan <Ahmed Khalaf Hassan@DESKTOP-BRBO08O>
2022-11-19 19:01:09 +0200

    maintain another conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
-<<<<<<< HEAD
 import { SideMenuComponent } from './Components/side-menu/side-menu.component';
-=======
 import { FooterComponent } from './Components/footer/footer.component';
->>>>>>> yusufhsn
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
-    SideMenuComponent
-=======
+    SideMenuComponent,
     FooterComponent
->>>>>>> yusufhsn
   ],
   imports: [
     BrowserModule,
