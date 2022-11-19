522106a33696415d760118f10e65d0ebdd616c6e
Ahmed Khalaf Hassan <Ahmed Khalaf Hassan@DESKTOP-BRBO08O>
2022-11-19 19:30:13 +0200

    modifiy AppModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
-import { SidebarComponent } from './Components/sidebar/sidebar.component';
-import { ContentComponent } from './Content/content/content.component';
+import { ContentComponent } from './Components/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent,
     ContentComponent
   ],
   imports: [
