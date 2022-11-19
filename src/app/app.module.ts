65e8ed119059caf5121ac435213ea33549d6f78b
yousefzakaria250 <yousefzakaria27@gmail.com>
2022-11-19 19:19:37 +0200

    Add Content Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { ContentComponent } from './Content/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
